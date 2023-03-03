ca26e625b5b629e52ccd715e9017a088dd6fd567
Tõnis <tonislille@gmail.com>
2023-03-03 04:11:16 +0200

    Create HouseServices

---
+++ ProjectHouse.ApplicationServices/Services/HouseServices.cs
@@ -0,0 +1,36 @@
+﻿using ProjectHouse.Core.Domain;
+using ProjectHouse.Core.Dto;
+using ProjectHouse.Data;
+
+namespace ProjectHouse.ApplicationServices.Services
+{
+    public class HouseServices
+    {
+        private readonly ProjectHouseContext _context;
+        public HouseServices(ProjectHouseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<House> Create(HouseDto dto)
+        {
+            House house = new House()
+            {
+                id = Guid.NewGuid(),
+                Size = dto.Size,
+                NumberOfFloors = dto.NumberOfFloors,
+                NumberOfBathrooms = dto.NumberOfBathrooms,
+                NumberOfBedrooms = dto.NumberOfBedrooms,
+                CreatedAt = DateTime.Now,
+                ModifiedAt = DateTime.Now
+
+            };
+
+            await _context.Houses.AddAsync(house);
+            await _context.SaveChangesAsync();
+
+            return house;
+        }
+    }
+}
+
