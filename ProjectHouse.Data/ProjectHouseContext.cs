1f4f4a409f9f366551722170b6bf714354490a8f
Tõnis <tonislille@gmail.com>
2023-03-03 02:42:32 +0200

    Create ProjectHouseContext

---
+++ ProjectHouse.Data/ProjectHouseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectHouse.Core.Domain;
+
+namespace ProjectHouse.Data
+{
+    internal class ProjectHouseContext :DbContext
+    {
+        public ProjectHouseContext(DbContextOptions<ProjectHouseContext> options)
+        : base(options) { }
+
+        public DbSet<House> Houses { get; set; }
+    }
+}
