e57fb10ecf81e81b5971c73335bcb2660397100e
Tõnis <tonislille@gmail.com>
2023-03-03 03:23:19 +0200

    Update HouseController

--- ProjectHouse/Controllers/House/HouseController.cs
+++ ProjectHouse/Controllers/House/HouseController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectHouse.Data;
+using ProjectHouse.Models.House;
 
 namespace ProjectHouse.Controllers.House
 {
     public class HouseController : Controller
     {
+        private readonly ProjectHouseContext _context;
+
+        public HouseController
+            (
+                ProjectHouseContext context
+            )
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var result = _context.Houses
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(x => new HouseIndexViewModel
+                {
+                    id = x.id,
+                    Size = x.Size,
+                    NumberOfFloors = x.NumberOfFloors,
+                    NumberOfBathrooms = x.NumberOfBathrooms,
+                    NumberOfBedrooms = x.NumberOfBedrooms
+                });
+            return View(result);
         }
     }
 }
