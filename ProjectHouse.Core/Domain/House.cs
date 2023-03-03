1ac9caf8306cf306c6cb97363fb8160b5ec512de
Tõnis <tonislille@gmail.com>
2023-03-03 02:31:53 +0200

    Create Domain House

---
+++ ProjectHouse.Core/Domain/House.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace ProjectHouse.Core.Domain
+{
+    public class House
+    {
+        [Key]
+        public Guid? id { get; set; }
+        public int Size { get; set; }
+        public int NumberOfFloors { get; set; }
+        public int NumberOfBathrooms { get; set; }
+        public int NumberOfBedrooms { get; set; }
+
+        // Only in database
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
