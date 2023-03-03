83b2e43153ab6ec50434c1c5bf794089499194d2
Tõnis <tonislille@gmail.com>
2023-03-03 03:04:43 +0200

    Create HouseController

---
+++ ProjectHouse/Controllers/House/HouseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectHouse.Controllers.House
+{
+    public class HouseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
