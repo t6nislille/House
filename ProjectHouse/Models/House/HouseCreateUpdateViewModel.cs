bff627c4fdcb457332381b6503ba5f1a6b39cc8f
Tõnis <tonislille@gmail.com>
2023-03-03 04:02:01 +0200

    Create HouseCreateUpdateViewModel

--- ProjectHouse/Models/House/HouseCreateUpdateViewModel.cs
+++ ProjectHouse/Models/House/HouseCreateUpdateViewModel.cs
@@ -2,5 +2,14 @@
 {
     public class HouseCreateUpdateViewModel
     {
+        public Guid? id { get; set; }
+        public int Size { get; set; }
+        public int NumberOfFloors { get; set; }
+        public int NumberOfBathrooms { get; set; }
+        public int NumberOfBedrooms { get; set; }
+
+        // Only in database
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
     }
 }
