556ee27d2e9de51a91fbf06d3b2325a5a3330f00
Tõnis <tonislille@gmail.com>
2023-04-05 20:01:20 +0300

    Services Fix

--- ProjectHouse.ApplicationServices/Services/HouseServices.cs
+++ ProjectHouse.ApplicationServices/Services/HouseServices.cs
@@ -1,4 +1,5 @@
-﻿using ProjectHouse.Core.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectHouse.Core.Domain;
 using ProjectHouse.Core.Dto;
 using ProjectHouse.Core.ServiceInterface;
 using ProjectHouse.Data;
@@ -15,22 +16,30 @@ namespace ProjectHouse.ApplicationServices.Services
 
         public async Task<House> Create(HouseDto dto)
         {
-            House house = new House()
-            {
-                id = Guid.NewGuid(),
-                Size = dto.Size,
-                NumberOfFloors = dto.NumberOfFloors,
-                NumberOfBathrooms = dto.NumberOfBathrooms,
-                NumberOfBedrooms = dto.NumberOfBedrooms,
-                CreatedAt = DateTime.Now,
-                ModifiedAt = DateTime.Now
+            House house = new House();
 
-            };
+            house.Id = Guid.NewGuid();
+            house.Size = dto.Size;
+            house.NumberOfFloors = dto.NumberOfFloors;
+            house.NumberOfBathrooms = dto.NumberOfBathrooms;
+            house.NumberOfBedrooms = dto.NumberOfBedrooms;
+            house.CreatedAt = DateTime.Now;
+            house.ModifiedAt = DateTime.Now;
+
+            
 
             await _context.Houses.AddAsync(house);
             await _context.SaveChangesAsync();
 
             return house;
+        }
+
+        public async Task<House> GetAsync(Guid id)
+        {
+            var result = await _context.Houses
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            return result;
         }
     }
 }
--- ProjectHouse.Core/ServiceInterface/IHouseServices.cs
+++ ProjectHouse.Core/ServiceInterface/IHouseServices.cs
@@ -11,5 +11,9 @@ namespace ProjectHouse.Core.ServiceInterface
     public interface IHouseServices
     {
         Task<House> Create(HouseDto dto);
+        Task<House> GetAsync(Guid id);
+       // Task<House> Update(HouseDto dto);
+       // Task<House> Delete(HouseDto dto);
+
     }
 }
