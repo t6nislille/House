cde2bb916ab205685ce4b432c016c9ac73971e6e
Tõnis <tonislille@gmail.com>
2023-04-05 20:32:27 +0300

    Add DetailsViewModel and DeleteViewmodel

---
+++ ProjectHouse/Models/House/HouseDeleteViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectHouse.Models.House
+{
+    public class HouseDeleteViewModel
+    {
+        public Guid? Id { get; set; }
+        public int Size { get; set; }
+        public int NumberOfFloors { get; set; }
+        public int NumberOfBathrooms { get; set; }
+        public int NumberOfBedrooms { get; set; }
+
+        // Only in database
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
